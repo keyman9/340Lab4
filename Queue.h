9774e4114011413a32917bb0c98cd635a0a4ecc7
Spencer S <laxscott@gmail.com>
2016-03-23 19:41:39 -0400

    Reorganization

--- Queue.h
+++ Queue.h
@@ -16,7 +16,7 @@ class Queue{
 		void setTail(Team* n){tail = n;}
 		void setHead(Team* n){head = n;}
 		void printQueue();
-		void nextLeg(string city,Team* slowest);
+		Team* nextLeg(string city);
 };
 
 void Queue::push(Team* n){
@@ -64,79 +64,16 @@ void Queue::printQueue(){
    	}
 }
 
-void Queue::nextLeg(string city, Team* slowest){
-	Queue tempQ = Queue();
-	cerr << "Temp queue contents: " << endl;
-	tempQ.printQueue();
-	cerr << "---End temp Queue--" << endl;
-	Team* firstTeam = NULL;
-    Team* nextTeam = NULL;
-	Team* fastest = NULL;
-    //function is still reordering the main queue
-	for(int i= size; i > 0; i--){
-		cerr << "!!!!!!!!!OUTER FOR BEGIN!!!!!!!!!!" << endl;
-		cerr << "First for begin" << endl;
-		fastest = pop();
-		firstTeam = fastest;						
-		cerr << fastest->getName() << " is fastest." << endl;
-		if(fastest->getCity() != city){
-			fastest->setTime(city);
-		}					
-		//set its time
-		cerr << "Time set." << endl;				
-		//Team time set this lap
-		int j = size -1;
-		while(j > 0){
-			cerr << "----------INNER FOR----------" <<endl;	
-			//# teams this round
-			cerr << "Teams still to run: " << j << endl;
-			cerr << "Fastest so far: " << fastest->getName() << endl;
-			nextTeam = peek();			
-			//next item for comparison
-			cerr << nextTeam->getName() << " peeked" << endl;
-			if(nextTeam->getCity() != city){		
-			//if the team hasn't had a new time generated this lap
-				nextTeam->setTime(city);			
-				//set a new time
-				cerr << "Time set." << endl;
-			}
-			if(!(fastest->ranFaster(nextTeam))){
-				cerr << "Fastest: " << fastest->getName() 
-					 << " was slower than " 
-					 << "nextTeam: " << nextTeam->getName() << endl;	
-				//if fastest is faster than next
-				push(fastest);			
-				//push the next team back onto the queue
-				cerr << "Original now contains prev fastest: " << fastest->getName() << endl;
-				fastest = pop();
-				j--;
-			}
-			else if(fastest->ranFaster(nextTeam)){ 
-				//if the next team is the fastest
-				if(j == 1){
-					tempQ.push(fastest);
-					cerr << fastest->getName() << " was the fastest team, pushed to the temp queue."  << endl;
-					fastest = pop();
-					j--;
-				}
-				else{
-					push(nextTeam);
-					cerr << nextTeam->getName() << " pushed back into original for being slow." << endl;								
-					j--;
-				}
-			}
-		}
+Team* Queue::nextLeg(string city){
+	Team* race = new Team[size];
+	int itemCount = size;
+	for(int i = 0; i < itemCount; i++){
+		Team temp = *pop();
+		temp.setTime(city);
+		race[i] = temp;
 	}
-	cerr << "Begin copying queues. " << endl;
-	Team* slow = NULL;
-	while(tempQ.peek() != NULL){	
-		//replace the original queue with the fastest times stored in the temp queue
-		slow = tempQ.pop();
-		if(tempQ.getSize() ==0 ){
-			slowest = slow;
-		}
-		else{
-			push(slow);
-		}
-	}	
+	///FUNCTION CALL
+	for(int i = 0; i < size; i++){
+		//push(race[i])	
+	} 	
 }
